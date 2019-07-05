14ed4529a1ff962770d7f79e88780904bc5f745d
Gnought <1684105+gnought@users.noreply.github.com>
2019-07-05 20:09:21 +0800

    WIP

--- mqemitter-redis.js
+++ mqemitter-redis.js
@@ -9,6 +9,10 @@ var msgpack = require('msgpack-lite')
 var EE = require('events').EventEmitter
 var assert = require('assert')
 
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(() => resolve('I did something'), milliseconds))
+}
+
 function MQEmitterRedis (opts) {
   if (!(this instanceof MQEmitterRedis)) {
     return new MQEmitterRedis(opts)
@@ -43,17 +47,21 @@ function MQEmitterRedis (opts) {
 
   function handler (sub, topic, payload) {
     var packet = msgpack.decode(payload)
+    that.pending -= 1
     if (!that._cache.get(packet.id)) {
+      // console.log(packet.msg, 'emit')
       that._emit(packet.msg)
     }
     that._cache.set(packet.id, true)
   }
 
   this.subConn.on('messageBuffer', function (topic, message) {
+    // console.log('received messageBuffer', topic)
     handler(topic, topic, message)
   })
 
   this.subConn.on('pmessageBuffer', function (sub, topic, message) {
+    // console.log('received pmessageBuffer', topic)
     handler(sub, topic, message)
   })
 
@@ -80,6 +88,9 @@ function MQEmitterRedis (opts) {
   MQEmitter.call(this, opts)
 
   this._opts.regexWildcardOne = new RegExp(this._opts.wildcardOne.replace(/([/,!\\^${}[\]().*+?|<>\-&])/g, '\\$&'), 'g')
+  this._opts.regexSep = new RegExp(this._opts.separator, 'g')
+  this.pending = 0
+  // this.concurrency = 1
 }
 
 inherits(MQEmitterRedis, MQEmitter)
@@ -91,36 +102,81 @@ inherits(MQEmitterRedis, MQEmitter)
 MQEmitterRedis.prototype.close = function (done) {
   var that = this
 
-  var subConnEnd = false
-  var pubConnEnd = false
-
+  // var subConnEnd = false
+  // var pubConnEnd = false
+  // console.log(this._id, 'close')
   var cleanup = function () {
-    if (subConnEnd && pubConnEnd) {
-      that._topics = {}
-      that._matcher.clear()
-      that._cache.prune()
-      that._close(done || nop)
-    }
+    // if (subConnEnd && pubConnEnd) {
+    console.log('cleanup')
+    that._topics = {}
+    that._matcher.clear()
+    that._cache.prune()
+    that._close(done || nop)
+    // }
   }
 
   var handleClose = function () {
-    process.nextTick(() => {
-      that.pubConn.quit(() => { that.pubConn.disconnect(false); pubConnEnd = true; cleanup() })
-      that.subConn.quit(() => { that.subConn.disconnect(false); subConnEnd = true; cleanup() })
-    })
+    // that.pubConn.removeListener(topic, handleClose)
+    that.removeListener(topic, handleClose)
+    while (that.pending > 0) {
+      sleep(1000)
+    }
+    // console.log('handleClose')
+    that.pubConn.disconnect(false)
+    that.subConn.disconnect(false)
+    // that.pubConn.quit(() => { pubConnEnd = true; })
+    // that.subConn.quit(() => { subConnEnd = true; })
+    // that.pubConn.quit()
+    // that.subConn.quit()
+    that.pubConn.quit().catch(nop)
+    that.subConn.quit(cleanup).catch(nop)
   }
+  // console.log(that.pending)
+  // if ( {
+  //   handleClose()
+  // }
+  // else {
+  //   setImmediate(() => {
+  //     setTimeout(handleClose, 1000)
+  //   })
+  // }
 
-  if (that.pubConn.status === 'ready') {
-    var sep = that._opts.separator
-    var topic = '$SYS' + sep + that._subTopic(that._id).replace(sep, '') + sep + 'redis' + sep + 'close'
-    this.on(topic, () => {
-      handleClose()
-    }, () => {
-      this.emit({ topic: topic, payload: 1 })
+  var sep = that._opts.separator
+  var topic = '$SYS' + sep + that._subTopic(that._id).replace(that._opts.regexSep, '') + sep + 'redis' + sep + 'close'
+  // that.pubConn.ping(() => { process.nextTick(() => { pubConnEnd = true; that.pubConn.disconnect(false); that.pubConn.quit(cleanup) }) })
+  // that.subConn.ping((err) => { console.log(err) })
+  // console.log(that.pending)
+  if (that.pending > 0 && that.subConn.status === 'ready') {
+    // that.subConn.ping(() => {
+    // that.pubConn.disconnect(false); that.pubConn.quit()
+    // that.subConn.disconnect(false); that.subConn.quit(cleanup)
+    that.on(topic, handleClose, () => {
+      process.nextTick(() => {
+        that.emit({ topic: topic, payload: 1 })
+      })
     })
+    // })
   } else {
+    console.log('handleClose_offline')
     handleClose()
+    // that.pubConn.disconnect(true)
+    // that.subConn.disconnect(true)
+    // that.pubConn.quit()
+    // that.subConn.quit(cleanup)
+    // process.nextTick(cleanup)
   }
+  // if (that.pending > 0 && that.pubConn.status === 'ready') {
+  //   var sep = that._opts.separator
+  //   var topic = '$SYS' + sep + that._subTopic(that._id).replace(sep, '') + sep + 'redis' + sep + 'close'
+  //   that.on(topic, () => {
+  //     that.removeListener(topic, handleClose)
+  //     handleClose()
+  //   }, () => {
+  //     this.emit({ topic: topic, payload: 1 })
+  //   })
+  // } else {
+  //   handleClose()
+  // }
 
   return this
 }
@@ -132,14 +188,18 @@ MQEmitterRedis.prototype._subTopic = function (topic) {
 }
 
 MQEmitterRedis.prototype.on = function on (topic, cb, done) {
+  console.log('on', topic)
   var subTopic = this._subTopic(topic)
   var onFinish = function () {
     if (done) {
       setImmediate(done)
+      // process.nextTick(done)
     }
   }
 
   this._on(topic, cb)
+
+  this.pending += 1
 
   if (this._topics[subTopic]) {
     this._topics[subTopic]++
@@ -165,16 +225,36 @@ MQEmitterRedis.prototype.emit = function (msg, done) {
     }
   }
 
-  var onFinish = function () {
-    if (done) {
-      setImmediate(done)
-    }
-  }
+  var that = this
+
   var packet = {
     id: hyperid(),
     msg: msg
   }
-  this.pubConn.publish(msg.topic, msgpack.encode(packet), onFinish)
+  var onFinish = function () {
+    if (done) {
+      var sep = that._opts.separator
+      var topic = '$SYS' + sep + that._subTopic(packet.id).replace(that._opts.regexSep, '') + sep + 'redis' + sep + 'emitCallback'
+      var handleDone = function () {
+        that.removeListener(topic, handleDone)
+        setTimeout(done)
+        // done()
+      }
+      that.on(topic, handleDone, () => {
+        that.emit({ topic: topic, payload: 1 })
+      })
+      // setImmediate(done)
+      // setTimeout(() => { setImmediate(done) })
+    }
+  }
+  // var onFinish = function () {
+  //   if (done) {
+  //     // setImmediate(done)
+  //     setTimeout(done)
+  //   }
+  // }
+
+  that.pubConn.publish(msg.topic, msgpack.encode(packet), onFinish)
 }
 
 MQEmitterRedis.prototype.removeListener = function (topic, cb, done) {
@@ -186,6 +266,8 @@ MQEmitterRedis.prototype.removeListener = function (topic, cb, done) {
   }
 
   this._removeListener(topic, cb)
+
+  this.pending -= 1
 
   if (--this._topics[subTopic] > 0) {
     onFinish()
